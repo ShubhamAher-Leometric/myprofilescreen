efe4bdbd1ff208534c6657ead59e13395619d56d
shubham aher <shubhamahersa25@gmail.com>
2024-07-16 12:04:49 +0530

    button issue resolved

--- lib/user_profile/profile_screen.dart
+++ lib/user_profile/profile_screen.dart
@@ -14,131 +14,149 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Center(
-          child: Text('Edit Profile',
-            style: TextStyle(fontSize: 16,
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            'Edit Profile',
+            style: TextStyle(
+                fontSize: 16,
                 fontWeight: FontWeight.w700,
-                color: apptextcolor2),),
-      ),
+                color: apptextcolor2),
+          ),
+        ),
       ),
       body: Column(
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 30,),
-              CircleAvatar(
-                radius: 50,
-                backgroundColor: Colors.red,
-                backgroundImage: AssetImage('assets/user_image.png'),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  SizedBox(height: 30,),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20.0,right: 20.0),
-                    child: Container(
-                      height: 70,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.withOpacity(0.2),
-                        border: Border.all(color: Colors.transparent),
-                        borderRadius: BorderRadius.circular(
-                            20),
-                      ),
-                      child: Row(
-                        children: [
-                          Spacer(),
-                          Container(
-                            width: 250,
-                            child: TextField(
-                              decoration: InputDecoration(
-                                hintText: 'Mathew Adam',
-                                border: InputBorder
-                                    .none, // Hide the default border of the text field
-                              ),
-                              keyboardType: TextInputType.text,
+          Container(height: MediaQuery.of(context).size.height/1.5,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                  height: MediaQuery.of(context).size.height/30,
+                ),
+                CircleAvatar(
+                  radius: 50,
+                  backgroundColor: Colors.red,
+                  backgroundImage: AssetImage('assets/user_image.png'),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height/30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: Container(
+                    height: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.2),
+                      border: Border.all(color: Colors.transparent),
+                      borderRadius: BorderRadius.circular(20),
+                    ),
+                    child: Row(
+                      children: [
+                        Spacer(),
+                        Container(
+                          width: 250,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Mathew Adam',
+                              border: InputBorder
+                                  .none, // Hide the default border of the text field
                             ),
+                            keyboardType: TextInputType.text,
                           ),
-                          Spacer(),
-                          Icon(Icons.perm_identity_rounded,),
-                          Spacer(),
-                        ],
-                      ),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.perm_identity_rounded,
+                        ),
+                        Spacer(),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 30,),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20.0,right: 20.0),
-                    child: Container(
-                      height: 70,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.withOpacity(0.2),
-                        border: Border.all(color: Colors.transparent), // Border color
-                        borderRadius: BorderRadius.circular(
-                            20), // Border radius to make it circular
-                      ),
-                      child: Row(
-                        children: [
-                          Spacer(),
-                          Container(
-                            width: 250,
-                            child: TextField(
-                              decoration: InputDecoration(
-                                hintText: '+62 112-3288-9111 ',
-                                border: InputBorder
-                                    .none, // Hide the default border of the text field
-                              ),
-                              keyboardType: TextInputType.text,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height/30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: Container(
+                    height: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.2),
+                      border: Border.all(color: Colors.transparent),
+                      // Border color
+                      borderRadius: BorderRadius.circular(
+                          20), // Border radius to make it circular
+                    ),
+                    child: Row(
+                      children: [
+                        Spacer(),
+                        Container(
+                          width: 250,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: '+62 112-3288-9111 ',
+                              border: InputBorder
+                                  .none, // Hide the default border of the text field
                             ),
+                            keyboardType: TextInputType.text,
                           ),
-                          Spacer(),
-                          Icon(Icons.local_phone_outlined,),
-                          Spacer(),
-                        ],
-                      ),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.local_phone_outlined,
+                        ),
+                        Spacer(),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 30,),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20.0,right: 20.0),
-                    child: Container(
-                      height: 70,
-                      decoration: BoxDecoration(
-                        color: Colors.grey.withOpacity(0.2),
-                        border: Border.all(color: Colors.transparent), // Border color
-                        borderRadius: BorderRadius.circular(
-                            20), // Border radius to make it circular
-                      ),
-                      child: Row(
-                        children: [
-                          Spacer(),
-                          Container(
-                            width: 250,
-                            child: TextField(
-                              decoration: InputDecoration(
-                                hintText: 'Mathew@email.com',
-                                border: InputBorder
-                                    .none, // Hide the default border of the text field
-                              ),
-                              keyboardType: TextInputType.text,
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height/30,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0, right: 20.0),
+                  child: Container(
+                    height: 70,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.withOpacity(0.2),
+                      border: Border.all(color: Colors.transparent),
+                      // Border color
+                      borderRadius: BorderRadius.circular(
+                          20), // Border radius to make it circular
+                    ),
+                    child: Row(
+                      children: [
+                        Spacer(),
+                        Container(
+                          width: 250,
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Mathew@email.com',
+                              border: InputBorder
+                                  .none, // Hide the default border of the text field
                             ),
+                            keyboardType: TextInputType.text,
                           ),
-                          Spacer(),
-                          Icon(Icons.mail_outline_outlined,),
-                          Spacer(),
-                        ],
-                      ),
+                        ),
+                        Spacer(),
+                        Icon(
+                          Icons.mail_outline_outlined,
+                        ),
+                        Spacer(),
+                      ],
                     ),
                   ),
-                  SizedBox(height: 60,),
-                  CustomElevatedButton(title: 'Update', color: appprimary,
-                      textColor: apptextcolor,
-                      size: MediaQuery.of(context).size.width/2,
-                      onPressed: (){})
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
+          ),
+          CustomElevatedButton(
+            title: 'Update',
+            color: appprimary,
+            textColor: apptextcolor,
+            size: MediaQuery.of(context).size.width / 2,
+            onPressed: () {},
           ),
         ],
       ),
