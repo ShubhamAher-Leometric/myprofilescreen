3a140be743372160b75e500985ed12e4ef7c1c52
shubham aher <shubhamahersa25@gmail.com>
2024-07-22 18:42:29 +0530

    video player integration done

--- lib/location_property/video_screen.dart
+++ lib/location_property/video_screen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:video_360/video_360.dart';
+import 'package:video_player/video_player.dart';
 
 class VideoScreen extends StatefulWidget {
   @override
@@ -7,53 +7,40 @@ class VideoScreen extends StatefulWidget {
 }
 
 class _VideoScreenState extends State<VideoScreen> {
-  String durationText = '';
-  String totalText = '';
-  late Video360Controller _controller;
+  late VideoPlayerController _controller;
 
-  void _onVideo360ViewCreated(Video360Controller controller) {
-    _controller = controller;
-    // You can perform additional setup here if needed
+  @override
+  void initState() {
+    super.initState();
+    // Initialize the video player controller with a video URL and set it to loop
+    _controller = VideoPlayerController.network(
+      'https://youtu.be/tS9854NKZ4I?si=ztOemwyRbVrV_ZCu', // Replace with your video URL
+    )
+      ..initialize().then((_) {
+        // Ensure the first frame is shown after the video is initialized
+        setState(() {});
+      })
+      ..setLooping(true) // Set the video to loop
+      ..play(); // Start playing the video
   }
 
   @override
   void dispose() {
-    _controller?.dispose();
+    _controller.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('360 Video View'),
-      ),
       body: Center(
-        child: Video360View(
-          onVideo360ViewCreated: _onVideo360ViewCreated,
-          url: 'https://youtu.be/hEdzv7D4CbQ?si=zHvGKL_JYaPYuViz',
-          onPlayInfo: (Video360PlayInfo info) {
-            setState(() {
-              durationText = info.duration.toString();
-              totalText = info.total.toString();
-            });
-          },
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Text('Duration: $durationText'),
-            Text('Total: $totalText'),
-          ],
-        ),
+        child: _controller.value.isInitialized
+            ? AspectRatio(
+          aspectRatio: _controller.value.aspectRatio,
+          child: VideoPlayer(_controller),
+        )
+            : CircularProgressIndicator(),
       ),
     );
   }
 }
-
-void main() => runApp(MaterialApp(
-  home: VideoScreen(),
-));
